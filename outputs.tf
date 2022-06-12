5fa3e2ac98c2f01fa88affe3bace793ab8c8ae74
phnorwood <patrick.h.norwood@gamil.com>
2022-06-12 10:08:17 -0400

    modified outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,12 @@ output "instance_ami" {
   value = aws_instance.ubuntu.ami
 }
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.ubuntu.id
 }
 
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.ubuntu.public_ip
+}
