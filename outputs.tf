79fc10415cb3b1a5313bd82a449901d366ad6688
phnorwood <patrick.h.norwood@gamil.com>
2022-06-12 10:12:47 -0400

    corrected outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -11,8 +11,3 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.ubuntu.public_ip
 }
-
-output "instance_name" {
-  description = "EC2 instance name"
-  value       = aws_instance.ubuntu.instance_name
-}
